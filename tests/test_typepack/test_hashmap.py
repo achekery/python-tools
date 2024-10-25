32eb6ea524f748dadbb816c68b0f571196d1a37a
achekery <6503526+achekery@users.noreply.github.com>
2024-10-25 11:49:02 -0700

    Add testpack

---
+++ tests/test_typepack/test_hashmap.py
@@ -0,0 +1,8 @@
+from python_tools.typepack import hashmap
+
+# pylint: disable=C0103:invalid-name
+
+def test_MyHashMap_SimpleLinkedListBuckets_LeetCode():
+    """Create class object."""
+    _ = hashmap.MyHashMap_SimpleLinkedListBuckets_LeetCode()
+    assert True
